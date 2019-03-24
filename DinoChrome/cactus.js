30cda31d0d6503daa4ad01a9c2a9f38e2ae37a17
Alessandro Vezzoli <48863556+Ozzev@users.noreply.github.com>
2019-03-24 20:09:32 +0100

    Update cactus.js

--- DinoChrome/cactus.js
+++ DinoChrome/cactus.js
@@ -1,19 +1,20 @@
 class Cactus{
 	
-	constructor(){
-		this.type = random(3);
+	constructor(posx, posy, speed){
+		this.type = Math.floor(random(3));
+		this.posx = posx;
+		this.posy = posy;
+		this.speed = speed;
 		this.height = (this.type+5)/3*100;
 		this.width = ((this.type+this.type/2)/(this.type/2+1)+1)*100;
-		this.posx = 100;
-		this.posy = 100;
-		this.collision=false;
 	}
 	
 	collisionDetect(){
-		if ((this.posx-this.width/2 <= dinosaur.posx+dinosaur.width/2) && (this.posy-this.height/2 <= dinosaur.posx+dinosaur.heigth/2)) this.collision=true;
+		if ((this.posx-this.width/2 <= dinosaur.posx+dinosaur.width/2) && (this.posy-this.height/2 <= dinosaur.posx+dinosaur.heigth/2)) return true;
+		else return false;
 	}
 	
 	goingForward(){
-		this.posx-=1;
+		this.posx -= this.speed;
 	}
 }
