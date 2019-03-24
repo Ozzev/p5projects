ec05c25a34ef03320ac4d95587acc06712a2d2ed
Riccardo Piantoni <48888991+Richypiants@users.noreply.github.com>
2019-03-24 20:59:10 +0100

    Update normal.js

--- DinoChrome/normal.js
+++ DinoChrome/normal.js
@@ -5,6 +5,8 @@ function setup()
   createCanvas(500, 500);
   d = new Dino(100, 200, 50, 100, 0.5);
   fill(255);
+  Cacty = new Cactus();
+  fill(128);
 }
 
 function draw()
@@ -12,6 +14,8 @@ function draw()
   background(0);
   d.update();
   d.show();
+  Cacty.show();
+	Cacty.update();
 }
 
 function mousePressed()
